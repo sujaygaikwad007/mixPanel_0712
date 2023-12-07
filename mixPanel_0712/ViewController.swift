1c363220d9df2e4aae3ca2d468889a2d0ed0c19d
Aniket Patil <aniket.p@reapmind.com>
2023-12-07 12:50:29 +0530

    Initial Commit

---
+++ mixPanel_0712/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mixPanel_0712
+//
+//  Created by Aniket Patil on 07/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
